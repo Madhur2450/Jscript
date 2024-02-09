76abc8af6b393fbda2fcdff3b66a770c0648af00
Madhur2450 <159065407+Madhur2450@users.noreply.github.com>
2024-02-09 15:38:03 +0000

    Container config done

---
+++ 01_basic/)1.js
@@ -0,0 +1,11 @@
+console.log("Madhur");
+const accountid = 1234
+let mailID = "madhur@gmail.com"
+var accountpass = "12345"
+city = "gorakhpur"
+state =
+
+// accountid = 9876  
+
+console.log("accountid");
+console.table([accountid, mailID, accountpass, city, state])
