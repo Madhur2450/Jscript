93d8099be05a76c95e6d72c8e1b098e76f29193d
Madhur2450 <159065407+Madhur2450@users.noreply.github.com>
2024-02-20 19:27:34 +0000

    commit

--- 01_basic/03_conversion_operations.js
+++ 01_basic/03_conversion_operations.js
@@ -28,9 +28,36 @@ let BooleanIsLoggedIn = Boolean(isLoggedIn)
 let somenumber = 33
 let stringNumber = String(somenumber)
 
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+//******************Operations******************//
+
+let value = 10
+let negValue = -value
+
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**3);
+// console.log(2%2);
+
+let srt1 = "hello"
+let srt2 = " Madhur"
+
+srt3 = srt1 + srt2
+// console.log(srt3);
 
 
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2"); // 1-number calculation, 2- then all will trit as string. 
+
+console.log(3 + 45 - 12 % 12 ); // wrong way (not practical way to write)
 
 
