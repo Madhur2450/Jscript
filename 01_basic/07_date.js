92fa3a708afc91ff38be6fdff468c6241b14725d
Madhur2450 <159065407+Madhur2450@users.noreply.github.com>
2024-02-26 19:02:51 +0000

    commit

---
+++ 01_basic/07_date.js
@@ -0,0 +1,16 @@
+// Dates
+
+let myDate = new Date()
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleDateString());
+
+console.log(typeof myDate);
+
+let mycreatedDate = new Date(2023, 0, 23)
+console.log(mycreatedDate.toDateString());
+
+
+
+
+
